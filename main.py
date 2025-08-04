df9a3ef872696665d4a4b963892fd5912c65d2c1
Ahmad Raza <71838833+ahmadraza741@users.noreply.github.com>
2025-08-04 14:37:56 +0530

    Update main.py
    added more domains

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from email.message import EmailMessage
 
 app = Flask(__name__)
 
-COMMON_DOMAINS = [".com", ".co.in", ".net", ".org", ".in"]
+COMMON_DOMAINS = [".com", ".co.in", ".net", ".org", ".in", ".inc", ".corp", ".tech", ".global", ".io", ".solutions", ".group", ".company", ".biz"]
 EMAIL_PATTERNS = [
     "{first}.{last}",
     "{first[0]}.{last}",
@@ -72,3 +72,4 @@ if __name__ == "__main__":
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
